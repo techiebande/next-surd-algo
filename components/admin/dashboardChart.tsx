0292583a07330ac048c0b94bfcce66440a07e5e7
techiebande <obande5826@gmail.com>
2024-09-30 20:53:38 +0100

    add admin area

--- components/admin/dashboardChart.tsx
+++ components/admin/dashboardChart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { TrendingUp } from "lucide-react";
-import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
 
 import {
   Card,
@@ -18,7 +18,7 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
-export const description = "An area chart with axes";
+export const description = "An area chart with gradient fill";
 
 const chartData = [
   { month: "January", desktop: 186, mobile: 80 },
@@ -44,7 +44,7 @@ export function DashboardChart() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Area Chart - Axes</CardTitle>
+        <CardTitle>Area Chart - Gradient</CardTitle>
         <CardDescription>
           Showing total visitors for the last 6 months
         </CardDescription>
@@ -55,7 +55,7 @@ export function DashboardChart() {
             accessibilityLayer
             data={chartData}
             margin={{
-              left: -20,
+              left: 12,
               right: 12,
             }}
           >
@@ -67,17 +67,37 @@ export function DashboardChart() {
               tickMargin={8}
               tickFormatter={(value) => value.slice(0, 3)}
             />
-            <YAxis
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickCount={3}
-            />
             <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <defs>
+              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-desktop)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-desktop)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+            </defs>
             <Area
               dataKey="mobile"
               type="natural"
-              fill="var(--color-mobile)"
+              fill="url(#fillMobile)"
               fillOpacity={0.4}
               stroke="var(--color-mobile)"
               stackId="a"
@@ -85,7 +105,7 @@ export function DashboardChart() {
             <Area
               dataKey="desktop"
               type="natural"
-              fill="var(--color-desktop)"
+              fill="url(#fillDesktop)"
               fillOpacity={0.4}
               stroke="var(--color-desktop)"
               stackId="a"
